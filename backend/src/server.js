6a22833e7fc299bfeb362d2e6c84bfaa19950d43
Calvin Nguyen <calvinnguyen50@gmail.com>
2026-03-19 22:40:32 -0700

    added cors to connect backend and front and tested to display json data in app.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const fs = require("fs");
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 const DATA_PATH = "./src/data/listings.json";
