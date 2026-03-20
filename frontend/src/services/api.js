5673e8fca16834ec95c114ed741cdac636b98863
Calvin Nguyen <calvinnguyen50@gmail.com>
2026-03-19 22:33:34 -0700

    created api layer inside frontend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,35 @@
+const BASE_URL = "http://localhost:3001";
+
+export const getListings = async () => {
+    const res = await fetch(`${BASE_URL}/listings`);
+    return res.json();
+};
+
+export const createListing = async (data) => {
+    const res = await fetch(`${BASE_URL}/listings`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+};
+
+export const placeBid = async (id, data) => {
+    const res = await fetch(`${BASE_URL}/listings/${id}/bid`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+};
+
+export const closeAuction = async (id) => {
+    const res = await fetch(`${BASE_URL}/listings/${id}/close`, {
+        method: "POST"
+    });
+    return res.json();
+};
