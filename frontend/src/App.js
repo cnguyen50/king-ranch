2e145c4259a3b225996224e7f2316d32e0918a21
Calvin Nguyen <calvinnguyen50@gmail.com>
2026-03-19 22:36:37 -0700

    fetched data in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { getListings } from "./services/api";
+
 function App() {
-  return <h1>King Ranch MVP</h1>;
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const data = await getListings();
+    setListings(data);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>King Ranch Auctions</h1>
+
+      {listings.map((listing) => (
+        <div key={listing.id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+          <h2>{listing.title}</h2>
+          <p>Current Price: ${listing.currentPrice}</p>
+          <p>Status: {listing.status}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
