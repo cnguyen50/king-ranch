f0d908adde145d7b3e8079e7944bd4ac8e04c7c0
Calvin Nguyen <calvinnguyen50@gmail.com>
2026-03-22 15:56:51 -0700

    added createListing into App.js with Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getListings } from "./services/api";
+import { Routes, Route } from "react-router-dom";
 import BidList from "./components/BidList";
 import NavBar from "./components/NavBar";
 import CreateListing from "./pages/CreateListing";
@@ -18,23 +19,37 @@ function App() {
 
   return (
     <>
-    <NavBar />
-    <div style={{ padding: "20px" }}>
-      <h1>King Ranch Auctions</h1>
+      <NavBar />
 
-      {listings.map((listing) => (
-        <div
-          key={listing.id}
-          style={{ border: "1px solid black", margin: "10px", padding: "10px" }}
-        >
-          <h2>{listing.title}</h2>
-          <p>Current Price: ${listing.currentPrice}</p>
-          <p>Status: {listing.status}</p>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ padding: "20px" }}>
+              <h1>King Ranch Auctions</h1>
 
-          <BidList bids={listing.bids} />
-        </div>
-      ))}
-    </div>
+              {listings.map((listing) => (
+                <div
+                  key={listing.id}
+                  style={{
+                    border: "1px solid black",
+                    margin: "10px",
+                    padding: "10px"
+                  }}
+                >
+                  <h2>{listing.title}</h2>
+                  <p>Current Price: ${listing.currentPrice}</p>
+                  <p>Status: {listing.status}</p>
+
+                  <BidList bids={listing.bids} />
+                </div>
+              ))}
+            </div>
+          }
+        />
+
+        <Route path="/create" element={<CreateListing />} />
+      </Routes>
     </>
   );
 }
