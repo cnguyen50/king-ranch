b8982e4133e55d69c2d439b5fa647a53448163e6
Calvin Nguyen <calvinnguyen50@gmail.com>
2026-03-22 15:39:22 -0700

    installed material ui and created NavBar component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <AppBar position="static">
+        <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            King Ranch
+            </Typography>
+
+            <Button color="inherit" component={Link} to="/">
+            Home
+            </Button>
+
+            <Button color="inherit" component={Link} to="/create">
+            Create Listing
+            </Button>
+        </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
